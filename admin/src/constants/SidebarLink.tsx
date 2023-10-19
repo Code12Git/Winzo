c3ccf6bcbc519b6dc70b6e8b8ea2a08ea78c6f5c
Code12Git <saxenasaksham46@gmail.com>
2023-10-19 12:15:48 +0530

    Test

---
+++ admin/src/constants/SidebarLink.tsx
@@ -0,0 +1,35 @@
+import { Payment, PermDataSettingOutlined } from "@mui/icons-material";
+import { LayoutDashboardIcon, User, SortDescIcon } from "lucide-react";
+
+export const sidebarLinks = [
+	{
+		id: 1,
+		icon: <LayoutDashboardIcon />,
+		route: "/",
+		label: "Dashboard",
+	},
+	{
+		id: 2,
+		icon: <User />,
+		route: "/user",
+		label: "User",
+	},
+	{
+		id: 3,
+		icon: <SortDescIcon />,
+		route: "/content",
+		label: "Content",
+	},
+	{
+		id: 4,
+		icon: <Payment />,
+		route: "/transactions",
+		label: "Transaction",
+	},
+	{
+		id: 5,
+		icon: <PermDataSettingOutlined />,
+		route: "/permissions",
+		label: "Permissions",
+	},
+];
